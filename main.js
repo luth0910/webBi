9b6bb9e717140951458e9d2c28dc917e46bd3122
luth0910 <m.ruhunul.luthfi@gmail.com>
2023-05-18 13:29:06 +0700

    fix some bugs

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         $(".fa-bars").removeClass("fa-times");
         $(".navbar").removeClass("nav-toggle");
 
-        if ($(window).scrollTop() > 20) {
+        if ($(window).scrollTop() > 10) {
             $(".header").css({ background: "#6C5CE7", "box-shadow": "0 .2rem .5rem rgba(0,0,0,.4)" });
         } else {
             $(".header").css({ background: "none", "box-shadow": "none" });
